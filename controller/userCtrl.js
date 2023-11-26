75ae3b7ae6a27deaa650bb9c20488b11b41cedcd
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2023-11-26 19:25:04 +0500

    updated controllers & routes: email, product, user

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -3,7 +3,9 @@ const asyncHandler=require('express-async-handler');
 const { generateToken } = require('../config/jwtToken');
 const validateMongoDbId = require('../utils/validateMongodbId');
 const { generateRefreshToken } = require('../config/refreshtoken');
+const crypto = require('crypto');
 const jwt=require('jsonwebtoken');
+const { sendEmail } = require('./emailCtrl');
 
 const createUser=asyncHandler(async(req,res)=> {
   const email=req.body.email;
@@ -186,4 +188,46 @@ const unblockUser=asyncHandler(async(req, res)=> {
   }
 });
 
-module.exports={createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updateaUser, blockUser, unblockUser, handleRefreshToken, logout};
+const updatePassword = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const newPassword = req.body.password; 
+  validateMongoDbId(_id);
+  const user = await User.findById(_id);
+  if (newPassword) {
+    user.password = newPassword; 
+    const updatedPassword = await user.save();
+    res.json(updatedPassword);
+  } else {
+    res.json(user);
+  }
+});
+
+const forgotPasswordToken=asyncHandler(async(req,res)=> {
+  const {email}=req.body;
+  const user=await User.findOne({email});
+  if (!user) throw new Error("User not found with this email");
+  try {
+    const token=await user.createPasswordResetToken();
+    await user.save();
+    const resetURL=`Hi, please follow this link to reset Your Password. This link is valid for 10 minutes from now. <a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</a>`;
+    const data= {
+      to: email,
+      text: "Hey User",
+      subject: "Forgot Password Link",
+      html: resetURL
+    };
+    sendEmail(data);
+    res.json(token);
+  }
+  catch(error) {
+    throw new Error(error);
+  }
+})
+
+const resetPassword=asyncHandler(async(req, res)=> {
+  const {password}=req.body;
+  const {token}=req.params;
+  const hashToken=crypto.createHash('sha256').update(token).digest('hex');
+})
+
+module.exports={createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updateaUser, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,14 +1,19 @@
 const express=require("express");
-const {createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updateaUser, blockUser, unblockUser, handleRefreshToken, logout}=require("../controller/userCtrl");
+const {createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updateaUser, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken}=require("../controller/userCtrl");
 const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware");
 const router=express.Router();
+
 router.post("/register", createUser);
+router.post('/forgot-password-token', forgotPasswordToken);
+
+router.put('/password', authMiddleware, updatePassword);
 router.post("/login", loginUserCtrl);
 router.get('/all-users', getallUser);
 router.get('/refresh', handleRefreshToken);
 router.get('/logout', logout);
 router.get('/:id', authMiddleware, isAdmin, getaUser);
 router.delete('/:id', deleteaUser);
+
 router.put('/edit-user', authMiddleware, updateaUser);
 router.put('/block-user/:id', authMiddleware, isAdmin, blockUser);
 router.put('/unblock-user/:id', authMiddleware, isAdmin, unblockUser);
